d1d53d812d967e1b751d700dbf2f58524ce7eb88
Marcos Bezerra <marcosvini.lb@gmail.com>
2026-01-06 14:06:18 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import AddTask from "./components/AddTask"
+import Tasks from "./components/Tasks"
+import  "./App.css"
+import { useState } from "react"
+
+function App(){
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar",
+      description: "Estudar programação para se tornar um desenvolvedor full stack.",
+      isCompleted: false ,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar ingês para se tornar fluente.",
+      isCompleted: false , 
+    },
+  ]);
+
+  function onTaskClick(taskId){
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId){
+        return{...task, isCompleted: !task.isCompleted};
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,21 @@
+import { ChevronRightIcon } from "lucide-react"
+
+function Tasks(props){
+    return(
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {props.tasks.map((tasks) => 
+                <li key={tasks.id} className="flex gap-2">
+                    <button 
+                        onClick={() => props.onTaskClick(tasks.id)} 
+                        className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${tasks.isCompleted && "line-through"}`}>
+                            {tasks.title}
+                    </button>
+                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+            )}
+        </ul>) 
+}
+
+export default Tasks
