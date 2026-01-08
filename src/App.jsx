adeb8320ec9f423f98d85af0352f5bc1a145dd6a
Marcos Bezerra <marcosvini.lb@gmail.com>
2026-01-08 10:24:15 -0300

    feat: Adicionar Tarefas
    Cria o componente de adicionar tarefas e suas descrições

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AddTask from "./components/AddTask"
 import Tasks from "./components/Tasks"
 import  "./App.css"
 import { useState } from "react"
+import {v4} from "uuid"
 
 function App(){
   const [tasks, setTasks] = useState([
@@ -19,6 +20,16 @@ function App(){
     },
   ]);
 
+  function onAddTaskSubmit(title, description){
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask])
+  }
+
   function onTaskClick(taskId){
     const newTasks = tasks.map(task => {
       if(task.id === taskId){
@@ -29,12 +40,17 @@ function App(){
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick (taskId){
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks)
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-        <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
     </div>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,17 +1,20 @@
-import { ChevronRightIcon } from "lucide-react"
+import { ChevronRightIcon, TrashIcon } from "lucide-react"
 
-function Tasks(props){
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
     return(
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-            {props.tasks.map((tasks) => 
+            {tasks.map((tasks) => 
                 <li key={tasks.id} className="flex gap-2">
                     <button 
-                        onClick={() => props.onTaskClick(tasks.id)} 
-                        className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${tasks.isCompleted && "line-through"}`}>
+                        onClick={() => onTaskClick(tasks.id)} 
+                        className={`bg-slate-400 w-full text-white p-2 rounded-md text-left cursor-pointer ${tasks.isCompleted && "line-through"}`}>
                             {tasks.title}
                     </button>
-                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                    <button className="bg-slate-400 p-2 rounded-md text-white cursor-pointer" >
                         <ChevronRightIcon />
+                    </button>
+                    <button onClick={()=> onDeleteTaskClick(tasks.id)} className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
+                        <TrashIcon />
                     </button>
                 </li>
             )}
