71b5cc7fc37d56f1a8bbbe884118da516ecfdb71
Marcos Bezerra <marcosvini.lb@gmail.com>
2026-01-10 11:56:14 -0300

    fix: centraliza div TaskPage.jsx

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,8 +7,8 @@ function TaskPage(){
     const title = searchParams.get("title")
     const  description = searchParams.get("description")
     return (
-        <div className="w-screen h-screen bg-slate-500  p-6">
-            <div className="2-[500px] space-y-4">
+        <div className="w-screen h-screen bg-slate-500  p-6 flex justify-center">
+            <div className="w-[500px] space-y-4">
                 <div className="flex  justify-center relative">
                     <button onClick={()=> navigate(-1)} className="absolute  left-0 top-0 bottom 0 text-slate-100 cursor-pointer">
                         <ChevronLeftIcon />
